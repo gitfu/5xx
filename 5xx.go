e82e0cbc1803ea9f157c0460d786b2ce4d0ffeb2
a <a@futronic.smack>
2019-04-09 13:20:45 -0400

    fmt

--- 5xx.go
+++ 5xx.go
@@ -4,8 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 var lc int
@@ -14,74 +14,72 @@ var etime float64
 var hosts []string
 var errors []int
 var totals []int
+
 type Site struct {
 	total, errors int
 }
 
 var m map[string]*Site
 
+func do(line string) {
+	stime := 1493969101.0
+	etime := 1493969102.0
+	floated := 0.0
+	lc += 1
+	fu := strings.Split(line, "|")
+	hostname := strings.TrimSpace(fu[2])
+	if _, ok := m[hostname]; ok {
 
-func do(line string){
-stime := 1493969101.0
-etime :=1493969102.0
-	floated :=0.0
-		lc +=1
-		fu:=strings.Split(line,"|")
-		hostname:=strings.TrimSpace(fu[2])
-		if _,ok := m[hostname];ok {
-			;;
-		}else{
-			m[hostname] = &Site{
-				0,0,
+	} else {
+		m[hostname] = &Site{
+			0, 0,
 		}
-		}
-		//t := m[hostname]
-	  floated,_ =strconv.ParseFloat((strings.TrimSpace(fu[0])), 64)
-		if etime > floated{
-		if floated >= stime{
-				m[hostname].total +=1
-				if strings.HasPrefix(fu[4]," 5"){
-				m[hostname].errors +=1
-		}
+	}
+	//t := m[hostname]
+	floated, _ = strconv.ParseFloat((strings.TrimSpace(fu[0])), 64)
+	if etime > floated {
+		if floated >= stime {
+			m[hostname].total += 1
+			if strings.HasPrefix(fu[4], " 5") {
+				m[hostname].errors += 1
+			}
 
 		}
-}
+	}
 }
 
 func main() {
-m = make(map[string]*Site)
-lc =0
+	m = make(map[string]*Site)
+	lc = 0
 
-//files :=[]string{"20.data","20a.data"}
-files :=[]string{"10m.data","10ma.data","fat.data","out.data","out1.data","out2.data","out3.data"}
-for _,f := range files {
-fmt.Println(f)
-file, err := os.Open(f)
-if err != nil {
-  fmt.Println(err)
-  return
-}
-defer file.Close()
+	//files :=[]string{"20.data","20a.data"}
+	files := []string{"10m.data", "10ma.data", "fat.data", "out.data", "out1.data", "out2.data", "out3.data"}
+	for _, f := range files {
+		fmt.Println(f)
+		file, err := os.Open(f)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
 
-scanner := bufio.NewScanner(file)
-scanner.Split(bufio.ScanLines)
+		scanner := bufio.NewScanner(file)
+		scanner.Split(bufio.ScanLines)
 
-for scanner.Scan() {
-do(scanner.Text())
+		for scanner.Scan() {
+			do(scanner.Text())
 
-}
-for k,_ := range m {
-	fmt.Printf(k)
-	e := float64(m[k].errors)
-	t := float64(m[k].total)
-	p := (e/t)*100.0
-	fmt.Println(" ",p)
+		}
+		for k, _ := range m {
+			fmt.Printf(k)
+			e := float64(m[k].errors)
+			t := float64(m[k].total)
+			p := (e / t) * 100.0
+			fmt.Println(" ", p)
 
+		}
 
+		fmt.Println(lc)
 	}
 
-fmt.Println(lc)
 }
-
-}
-
